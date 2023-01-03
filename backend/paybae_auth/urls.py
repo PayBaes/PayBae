9826acae43300453344cec8bf1b7d9de0449ec87
ShauryaPanwar <panwarshaurya11@gmail.com>
2023-01-03 21:26:05 +0530

    signup api made

--- backend/paybae_auth/urls.py
+++ backend/paybae_auth/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
+from django.conf.urls import include
 
+router=DefaultRouter()
+router.register("", views.UserViewSet)
 urlpatterns = [
-    path('HelloDjango', views.HelloDjango, name='HelloDjango'),
+    path('api-auth/', include('rest_framework.urls'))
 ]
+
+urlpatterns +=router.urls
--- backend/paybae_auth/views.py
+++ backend/paybae_auth/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from rest_framework.response import Response 
+from rest_framework import viewsets, serializers
+from django.contrib.auth.models import User 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 # Create your views here.
@@ -10,3 +12,60 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 def HelloDjango(request):
     print("Hello Django!")
     return Response({"message": "Hello Django!"}, status=200)
+
+
+#SignUp
+
+MIN_LENGTH=8
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        min_length=MIN_LENGTH,
+        error_messages={
+            "min_length": f"Password must be longer than {MIN_LENGTH} characters."
+        },
+
+    )
+
+
+    password2 = serializers.CharField(
+        write_only=True,
+        min_length=MIN_LENGTH,
+        error_messages={
+            "min_length": f"Password must be longer than {MIN_LENGTH} characters."
+        },
+
+    )
+
+
+    class Meta:
+        model=User
+        fields="__all__"
+
+    def validate(self, data):
+      if data["password"] != data["password2"]:
+        raise serializers.ValidationError("Password does not match.")
+        return data
+
+    def create(self,validated_data):
+        user=User.objects.create(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            active=validated_data["active"]
+        )
+
+        user.set_password(validated_data["password"])
+        user.save()
+
+
+        return user
+
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer       
+    
